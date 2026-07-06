0aec13828517fa1d0b6bef2998e97678acde9a9c
Coder Avi <coder.avi.aryan@gmail.com>
2026-07-06 19:21:16 +0530

    Add files via upload

---
+++ get_vendor_summary.py
@@ -0,0 +1,153 @@
+
+import pandas as pd
+import logging
+from ingestion_db import ingest_db
+
+
+logging.basicConfig(
+    filename="logs/get_vendor_summary.log",
+    level=logging.DEBUG,
+    format="%(asctime)s-%(levelname)s-%(message)s",
+    filemode="a"
+)
+
+def create_vendor_summary(conn):
+    query = """
+    WITH FreightSummary AS (
+        SELECT
+            "VendorNumber",
+            SUM("Freight") AS "FreightCost"
+        FROM vendor_invoice
+        GROUP BY "VendorNumber"
+    ),
+    PurchaseSummary AS (
+        SELECT
+            p."VendorNumber",
+            p."VendorName",
+            p."Brand",
+            p."Description",
+            p."PurchasePrice",
+            pp."Price" AS "ActualPrice",
+            pp."Volume",
+            SUM(p."Quantity") AS "TotalPurchaseQuantity",
+            SUM(p."Dollars") AS "TotalPurchaseDollars"
+        FROM purchases p
+        JOIN purchase_prices pp
+            ON p."Brand" = pp."Brand"
+        WHERE p."PurchasePrice" > 0
+        GROUP BY
+            p."VendorNumber",
+            p."VendorName",
+            p."Brand",
+            p."Description",
+            p."PurchasePrice",
+            pp."Price",
+            pp."Volume"
+    ),
+    SalesSummary AS (
+        SELECT
+            "VendorNo",
+            "Brand",
+            SUM("SalesQuantity") AS "TotalSalesQuantity",
+            SUM("SalesDollars") AS "TotalSalesDollars",
+            SUM("SalesPrice") AS "TotalSalesPrice",
+            SUM("ExciseTax") AS "TotalExciseTax"
+        FROM sales
+        GROUP BY "VendorNo", "Brand"
+    )
+    SELECT
+        ps.*,
+        ss."TotalSalesQuantity",
+        ss."TotalSalesDollars",
+        ss."TotalSalesPrice",
+        ss."TotalExciseTax",
+        fs."FreightCost"
+    FROM PurchaseSummary ps
+    LEFT JOIN SalesSummary ss
+        ON ps."VendorNumber" = ss."VendorNo"
+        AND ps."Brand" = ss."Brand"
+    LEFT JOIN FreightSummary fs
+        ON ps."VendorNumber" = fs."VendorNumber"
+    ORDER BY ps."TotalPurchaseDollars" DESC
+    """
+
+    return pd.read_sql_query(query, conn)
+
+def clean_data(df):
+
+    df["Volume"] = pd.to_numeric(
+        df["Volume"],
+        errors="coerce"
+    )
+
+    df.fillna(0, inplace=True)
+
+    df["VendorName"] = df["VendorName"].astype(str).str.strip()
+    df["Description"] = df["Description"].astype(str).str.strip()
+
+    df["GrossProfit"] = (
+        df["TotalSalesDollars"]
+        - df["TotalPurchaseDollars"]
+    )
+
+    df["ProfitMargin"] = (
+        df["GrossProfit"]
+        / df["TotalSalesDollars"].replace(0, 1)
+    ) * 100
+
+    df["SalesToPurchaseRatio"] = (
+        df["TotalSalesDollars"]
+        / df["TotalPurchaseDollars"].replace(0, 1)
+    )
+
+    return df
+
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="inventory",
+    user="postgres",
+    password="0000",
+    port="5432"
+)
+
+from sqlalchemy import create_engine
+
+engine = create_engine(
+    "postgresql+psycopg2://postgres:0000@localhost:5432/inventory"
+)
+
+summary_df = create_vendor_summary(engine)
+
+clean_df = clean_data(summary_df)
+
+ingest_db(
+    clean_df,
+    "vendor_sales_summary",
+    engine
+)
+    
+if __name__ == "__main__":
+
+    logging.info("Creating Vendor Summary Table...")
+
+    summary_df = create_vendor_summary(engine)
+
+    logging.info(summary_df.head())
+
+    logging.info("Cleaning Data...")
+
+    clean_df = clean_data(summary_df)
+
+    logging.info(clean_df.head())
+
+    logging.info("Ingesting Data...")
+
+    ingest_db(
+        clean_df,
+        "vendor_sales_summary",
+        engine
+    )
+
+    logging.info("Completed")
